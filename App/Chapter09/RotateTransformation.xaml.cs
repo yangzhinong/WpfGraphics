80c954ff1d2addd468dbe9ccabbd78b265d78437
yangzhinong <66845701@qq.com>
2024-12-06 14:48:06 +0800

    Cube

--- App/Chapter09/RotateTransformation.xaml.cs
+++ App/Chapter09/RotateTransformation.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Media.Media3D;
-using System.Windows.Shapes;
 
 namespace WpfApp7.Chapter09
 {
@@ -67,7 +56,7 @@ namespace WpfApp7.Chapter09
             myTransform.CenterX = rotateCenter.X;
             myTransform.CenterY = rotateCenter.Y;
             myTransform.CenterZ = rotateCenter.Z;
-            myTransform.Rotation =   new AxisAngleRotation3D(rotateAxis, rotateAngle);
+            myTransform.Rotation = new AxisAngleRotation3D(rotateAxis, rotateAngle);
         }
     }
 }
