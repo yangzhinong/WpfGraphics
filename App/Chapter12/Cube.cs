36c46cbbd04841a03b4d91cbe083c025b621bc99
yangzhinong <66845701@qq.com>
2024-12-10 16:00:22 +0800

    2

--- App/Chapter12/Cube.cs
+++ App/Chapter12/Cube.cs
@@ -11,55 +11,35 @@ namespace Shapes
         {
             GeometryModel3D model = new GeometryModel3D();
             CubeGeometry geometry = new CubeGeometry();
-            geometry.Width = Width;
-            geometry.Length = Length;
-            geometry.Height = Height;
-            geometry.Center = Center;
 
+            geometry.Width = Width;
+            geometry.Height = Height;
+            geometry.Length = Length;
+            geometry.Center = Center;
             model.Geometry = geometry.Mesh3D;
-            model.Material = Material;
+            model.Material= Material;
 
             Model = model;
         }
 
-        // The Model property for the cube:
-        private static readonly DependencyProperty ModelProperty =
-            DependencyProperty.Register("Model",
-                                        typeof(Model3D),
-                                        typeof(Cube),
-                                        new PropertyMetadata(ModelPropertyChanged));
-
-        private static void ModelPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            Cube cube = (Cube)d;
-            cube.Visual3DModel = (Model3D)e.NewValue;
-        }
-
-        private Model3D Model
+        public Model3D Model
         {
             get { return (Model3D)GetValue(ModelProperty); }
             set { SetValue(ModelProperty, value); }
         }
 
-        // The material of the cube:
-        public static readonly DependencyProperty MaterialProperty =
-            DependencyProperty.Register("Material",
-                                        typeof(Material),
-                                        typeof(Cube),
-                                        new PropertyMetadata(new DiffuseMaterial(Brushes.Blue), PropertyChanged));
-
-        public Material Material
+        // The Model property for the cube:
+        private static readonly DependencyProperty
+        ModelProperty = DependencyProperty.Register("Model", typeof(Model3D), typeof(Cube),
+                        new PropertyMetadata(ModelPropertyChanged));
+        private static void ModelPropertyChanged(
+        DependencyObject d,
+        DependencyPropertyChangedEventArgs e)
         {
-            get { return (Material)GetValue(MaterialProperty); }
-            set { SetValue(MaterialProperty, value); }
+            Cube cube = (Cube)d;
+            cube.Visual3DModel = (Model3D)e.NewValue;
         }
 
-        // The side length of the Cube:
-        public static readonly DependencyProperty LengthProperty =
-            DependencyProperty.Register("Length",
-                                        typeof(double),
-                                        typeof(Cube),
-                                        new PropertyMetadata(1.0, PropertyChanged));
 
         public double Length
         {
@@ -67,12 +47,12 @@ namespace Shapes
             set { SetValue(LengthProperty, value); }
         }
 
-        // The side width of the Cube:
-        public static readonly DependencyProperty WidthProperty =
-            DependencyProperty.Register("Width",
-                                        typeof(double),
-                                        typeof(Cube),
-                                        new PropertyMetadata(1.0, PropertyChanged));
+        public static readonly DependencyProperty LengthProperty =
+            DependencyProperty.Register("Length", typeof(double), typeof(Cube),
+                new PropertyMetadata(1.0d, PropertyChanged));
+
+
+
 
         public double Width
         {
@@ -80,12 +60,11 @@ namespace Shapes
             set { SetValue(WidthProperty, value); }
         }
 
-        // The side height of the Cube:
-        public static readonly DependencyProperty HeightProperty =
-            DependencyProperty.Register("Height",
-                                        typeof(double),
-                                        typeof(Cube),
-                                        new PropertyMetadata(1.0, PropertyChanged));
+        public static readonly DependencyProperty WidthProperty =
+            DependencyProperty.Register("Width", typeof(double), typeof(Cube),
+                new PropertyMetadata(1.0d, PropertyChanged));
+
+
 
         public double Height
         {
@@ -93,12 +72,12 @@ namespace Shapes
             set { SetValue(HeightProperty, value); }
         }
 
-        // The center of the cube:
-        public static readonly DependencyProperty CenterProperty =
-            DependencyProperty.Register("Center",
-                                        typeof(Point3D),
-                                        typeof(Cube),
-                                        new PropertyMetadata(new Point3D(0.0, 0.0, 0.0), PropertyChanged));
+        public static readonly DependencyProperty HeightProperty =
+            DependencyProperty.Register("Height", typeof(double), typeof(Cube), new PropertyMetadata(1.0d, PropertyChanged));
+
+
+
+
 
         public Point3D Center
         {
@@ -106,11 +85,25 @@ namespace Shapes
             set { SetValue(CenterProperty, value); }
         }
 
+        public static readonly DependencyProperty CenterProperty =
+            DependencyProperty.Register("Center", typeof(Point3D), typeof(Cube),
+                new PropertyMetadata(new Point3D(0, 0, 0), PropertyChanged));
+
+
+
+        public Material Material
+        {
+            get { return (Material)GetValue(MaterialProperty); }
+            set { SetValue(MaterialProperty, value); }
+        }
+
+        public static readonly DependencyProperty MaterialProperty =
+            DependencyProperty.Register("Material", typeof(Material), typeof(Cube),
+                new PropertyMetadata(new DiffuseMaterial(Brushes.Blue), PropertyChanged));
 
         private static void PropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            Cube cube = (Cube)d;
-            cube.InvalidateModel();
+            (d as Cube)?.InvalidateModel();
         }
     }
 }
