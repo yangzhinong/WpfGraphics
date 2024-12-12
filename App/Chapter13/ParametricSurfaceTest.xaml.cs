80ddac2c33ee06eb5cd00bbbd230769bd8840006
yangzhinong <66845701@qq.com>
2024-12-12 11:23:47 +0800

    chapter13

---
+++ App/Chapter13/ParametricSurfaceTest.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Shapes;
+using WpfApp7.Chapter10;
+
+namespace WpfApp7.Chapter13
+{
+    /// <summary>
+    /// Interaction logic for ParametricSurfaceTest.xaml
+    /// </summary>
+    public partial class ParametricSurfaceTest : Window
+    {
+        private ParametricSurface ps = new ParametricSurface();
+        public ParametricSurfaceTest()
+        {
+            InitializeComponent();
+            ps.IsHiddenLine = false;
+            ps.Viewport3d = viewport;
+            //AddHelicoid();
+            //AddSphere();
+            //AddTorus();
+            //AddHyperboloid();
+            AddEllipticCylinder();
+        }
+        private void AddHelicoid()
+        {
+            ps.Umin = 0.5;
+            ps.Umax = 1;
+            ps.Vmin = -3 * Math.PI;
+            ps.Vmax = 3 * Math.PI;
+            ps.Nv = 100;
+            ps.Nu = 10;
+            ps.Ymin = ps.Vmin;
+            ps.Ymax = ps.Vmax;
+            ps.CreateSurface(Helicoid);
+        }
+
+        private Point3D Helicoid(double u, double v)
+        {
+            double x = u * Math.Cos(v);
+            double z = u * Math.Sin(v);
+            double y =  v;
+            return new Point3D(x, y, z);
+        }
+
+        private void AddSphere()
+        {
+            ps.Umin = 0;
+            ps.Umax = 2 * Math.PI;
+            ps.Vmin = -0.5 * Math.PI;
+            ps.Vmax = 0.5 * Math.PI;
+            ps.Nu = 20;
+            ps.Nv = 20;
+            ps.CreateSurface(Sphere);
+        }
+
+        private Point3D Sphere(double u, double v)
+        {
+            double x = Math.Cos(v) * Math.Cos(u);
+            double z = Math.Cos(v) * Math.Sin(u);
+            double y = Math.Sin(v);
+            return new Point3D(x, y, z);
+        }
+        private void AddTorus()
+        {
+            ps.Umin = 0;
+            ps.Umax = 2 * Math.PI;
+            ps.Vmin = 0;
+            ps.Vmax = 2 * Math.PI;
+            ps.Nu = 50;
+            ps.Nv = 20;
+            ps.CreateSurface(Torus);
+        }
+
+        private Point3D Torus(double u, double v)
+        {
+            double x = (1 + 0.3 * Math.Cos(v)) * Math.Cos(u);
+            double z = (1 + 0.3 * Math.Cos(v)) * Math.Sin(u);
+            double y = 0.3 * Math.Sin(v);
+            return new Point3D(x, y, z);
+        }
+
+        private void AddHyperboloid()
+        {
+            ps.Umin = -2 * Math.PI;
+            ps.Umax = 2 * Math.PI;
+            ps.Vmin = -2 * Math.PI;
+            ps.Vmax = 2 * Math.PI;
+            ps.Nu = 50;
+            ps.Nv = 50;
+            ps.CreateSurface(Hyperboloid);
+        }
+
+        /// <summary>
+        /// 双曲面
+        /// </summary>
+        /// <param name="u"></param>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        private Point3D Hyperboloid(double u, double v)
+        {
+            const double a = 1.0d, b = 1.0d, c = 1.0d;
+            double x = a * Math.Cos(u) * Math.Cosh(v);
+            double y = b * Math.Sinh(v);
+            double z = c * Math.Sin(u) * Math.Cosh(v);
+            return new Point3D(x, y, z);
+        }
+
+        private void AddEllipticCylinder()
+        {
+            ps.Umin = 0;
+            ps.Umax = 2 * Math.PI;
+            ps.Vmin = -1;
+            ps.Vmax = 1;
+            ps.Nu = 30;
+            ps.Nv = 10;
+            ps.CreateSurface(EllipticCylinder);
+        }
+        /// <summary>
+        /// 椭圆柱面
+        /// </summary>
+        /// <param name="u"></param>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        private Point3D EllipticCylinder(double u, double v)
+        {
+            const double a = 1.5d, b = 1.0d;
+            double x = a * Math.Cos(u);
+            double y = v;
+            double z = b * Math.Sin(u);
+            return new Point3D(x, y, z);
+        }
+
+    }
+}
---
+++ App/Chapter13/SimpleSurfaceTest.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Shapes;
+
+namespace WpfApp7.Chapter13
+{
+    /// <summary>
+    /// Interaction logic for SimpleSurfaceTest.xaml
+    /// </summary>
+    public partial class SimpleSurfaceTest : Window
+    {
+        private SimpleSurface ss = new SimpleSurface();
+        public SimpleSurfaceTest()
+        {
+            InitializeComponent();
+            ss.IsHiddenLine = false;
+            ss.Viewport3d = viewport;
+            //AddSinc();
+            //AddPeaks();
+            AddRandomSurface();
+        }
+
+        private void AddSinc()
+        {
+            ss.Xmin = -8;
+            ss.Xmax = 8;
+            ss.Zmin = -8;
+            ss.Zmax = 8;
+            ss.Ymin = -1;
+            ss.Ymax = 1;
+            ss.CreateSurface(Sinc);
+        }
+
+        private Point3D Sinc(double x, double z)
+        {
+            double r = Math.Sqrt(x * x + z * z) + 0.00001;
+            double y = Math.Sin(r) / r;
+            return new Point3D(x, y, z);
+        }
+
+        private void AddPeaks()
+        {
+            ss.Xmin = -3;
+            ss.Xmax = 3;
+            ss.Zmin = -3;
+            ss.Zmax = 3;
+            ss.Ymin = -8;
+            ss.Ymax = 8;
+            ss.CreateSurface(Peaks);
+        }
+
+        private Point3D Peaks(double x, double z)
+        {
+            double y = 3 * Math.Pow((1 - x), 2) * Math.Exp(-x * x - (z + 1) * (z + 1)) -
+                            10 * (0.2 * x - Math.Pow(x, 3)-Math.Pow(z, 5)) * 
+                            Math.Exp(-x * x - z * z) -1 / 3 * Math.Exp(-(x + 1) * (x + 1) - z * z);
+            return new Point3D(x, y, z);
+        }
+
+        private void AddRandomSurface()
+        {
+            ss.Xmin = -8;
+            ss.Xmax = 8;
+            ss.Zmin = -8;
+            ss.Zmax = 8;
+            ss.Ymin = -1;
+            ss.Ymax = 1;
+            ss.CreateSurface(RandomSurface);
+        }
+        private Random rand = new Random();
+        private Point3D RandomSurface(double x, double z)
+        {
+            double r = Math.Sqrt(x * x + z * z) + 0.00001;
+            double y = Math.Sin(r) / r +  0.2 * rand.NextDouble();
+            return new Point3D(x, y, z);
+        }
+    }
+}
